a63af14fa925bb6ffe95230e17541a57c89180e7
Arthur <Arthur arthur.henriqu3@gmail.com>
2021-11-24 09:31:11 -0300

    First commit

---
+++ Papiro/src/main/java/br/com/aretwo/papiro/Escola1Application.java
@@ -0,0 +1,13 @@
+package br.com.aretwo.papiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Escola1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Escola1Application.class, args);
+	}
+
+}
---
+++ Papiro/src/test/java/br/com/aretwo/papiro/Escola1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aretwo.papiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Escola1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
